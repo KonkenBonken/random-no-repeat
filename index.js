cff9c486bf957cd102ad5052af1c407b75ddc405
KonkenBonken <konrad05pettersson@gmail.com>
2022-03-19 15:37:48 +0100

    Removed a dependency and added length argument to String generator

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createRandomString = exports.createRandomInt = exports.createRandomFloat = exports.createFunction = void 0;
-const { nanoid: randomString } = require('nanoid');
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const random = {
-    string: () => randomString(),
+    string: (_, length) => Array(length).fill(0).map(() => alphabet[Math.floor(Math.random() * (alphabet.length))]).join(''),
     float: (min, max) => Math.random() * (max - min) + min,
     int: (min, max) => Math.floor(Math.random() * (max + 1 - min) + min)
 };
-const createFunction = (type = 'float', min = 0, max = 10, used = []) => {
+const createFunction = (type = 'float', min = 0, max /*length*/ = 10, used = []) => {
     if (!random.hasOwnProperty(type))
         return;
     const randomFunction = random[type];
     return () => {
         if (type == 'int' && used.length >= (max - min + 1))
+            return;
+        else if (type == 'string' && used.length >= alphabet.length ** max)
             return;
         do {
             var result = randomFunction(min, max);
@@ -22,7 +24,7 @@ const createFunction = (type = 'float', min = 0, max = 10, used = []) => {
     };
 };
 exports.createFunction = createFunction;
-const createRandomFloat = (min, max, used) => createFunction('float', min, max, used), createRandomInt = (min, max, used) => createFunction('int', min, max, used), createRandomString = (used) => createFunction('string', undefined, undefined, used);
+const createRandomFloat = (min, max, used) => createFunction('float', min, max, used), createRandomInt = (min, max, used) => createFunction('int', min, max, used), createRandomString = (length, used) => createFunction('string', undefined, length, used);
 exports.createRandomFloat = createRandomFloat;
 exports.createRandomInt = createRandomInt;
 exports.createRandomString = createRandomString;
--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
-const { nanoid: randomString } = require('nanoid');
-
 type RandomTypes = 'float' | 'int' | 'string';
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const random = {
-  string: (): string => randomString(),
+  string: (_: any, length: number): string => Array(length).fill(0).map(() => alphabet[Math.floor(Math.random() * (alphabet.length))]).join(''),
   float: (min: number, max: number): number => Math.random() * (max - min) + min,
   int: (min: number, max: number): number => Math.floor(Math.random() * (max + 1 - min) + min)
 };
 
-const createFunction = (type: RandomTypes = 'float', min: number = 0, max: number = 10, used: Array<number | string> = []) => {
+const createFunction = (type: RandomTypes = 'float', min: number = 0, max/*length*/: number = 10, used: Array<number | string> = []) => {
   if (!random.hasOwnProperty(type)) return;
   const randomFunction = random[type];
   return (): number | string | void => {
     if (type == 'int' && used.length >= (max - min + 1)) return;
+    else if (type == 'string' && used.length >= alphabet.length ** max) return;
     do {
       var result = randomFunction(min, max);
     } while (used.includes(result));
@@ -22,7 +22,7 @@ const createFunction = (type: RandomTypes = 'float', min: number = 0, max: numbe
 };
 const createRandomFloat = (min?: number, max?: number, used?: number[]) => createFunction('float', min, max, used),
   createRandomInt = (min?: number, max?: number, used?: number[]) => createFunction('int', min, max, used),
-  createRandomString = (used?: string[]) => createFunction('string', undefined, undefined, used)
+  createRandomString = (length: number, used?: string[]) => createFunction('string', undefined, length, used)
 
 export default createFunction;
 export {
